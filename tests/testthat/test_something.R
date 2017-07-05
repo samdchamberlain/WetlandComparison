ad376a2d2c427d8fdbac43454891f6535a7ba267
samdchamberlain <sam.d.chamberlain@gmail.com>
2017-07-05 15:35:53 -0700

    try a real test??

---
+++ tests/testthat/test_something.R
@@ -0,0 +1,6 @@
+library("knitr")
+#knit(system.file("vignettes", "intro.Rmd", package="WetlandComparison"))
+#knit(system.file("vignettes", "full_analysis.Rmd", package="WetlandComparison"))
+## No "Error" should appear
+#expect_equal(length(grep("Error", readLines("intro.html"))), 0)
+#expect_equal(length(grep("Error", readLines("full_analysis.html"))), 0)
